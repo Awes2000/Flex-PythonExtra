c099e3a15beba277854cd5fa9259c2144230476d
Awes <28003@ma-web.nl>
2022-10-06 12:06:32 +0200

    turtleeee

---
+++ myTurtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.pencolor("red")
+turtle.fillcolor("white")
+turtle.begin_fill()
+
+for x in range(100):
+
+    turtle.forward(100)
+    turtle.right(45)
+    turtle.forward(100)
+    turtle.right(30)
+    turtle.forward(50)
+    turtle.forward(100)
+    turtle.right(45)
+    turtle.forward(100)
+    turtle.right(30)
+    turtle.forward(50)
+    turtle.left(25)
+    turtle.forward(120)
+    turtle.right(45)
+    turtle.forward(70)
+    turtle.right(45)
